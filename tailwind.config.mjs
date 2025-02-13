0b0fdc64298b7dfabd239145ba99e0730e8fdda0
Behroz Azhar <behrozazhar006@gamil.com>
2025-02-13 15:58:27 +0500

    first commit

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,9 +7,21 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "custom-gradient":
+          "linear-gradient(90deg, rgba(135, 17, 193, 0.3) 0%, rgba(36, 114, 252, 0.3) 100%)",
+      },
+      fontFamily: {
+        sans: ['"SF Pro Display"', "sans-serif"],
+      },
+      keyframes: {
+        wave: {
+          "0%, 100%": { transform: "rotate(0deg)" },
+          "50%": { transform: "rotate(15deg)" },
+        },
+      },
+      animation: {
+        wave: "wave 1s ease-in-out infinite",
       },
     },
   },
